8e022377c1d866bf51906b9a31f13fb85010d03f
lshep <lori.shepherd@roswellpark.org>
2024-11-25 08:01:14 -0500

    user requested deprecation

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,10 @@
 }
 
 .onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.22")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
   ## show vignette in windows menu
   if(.Platform$OS.type=="windows" && interactive() && .Platform$GUI=="Rgui"){
     addVigs2WinMenu("girafe")
